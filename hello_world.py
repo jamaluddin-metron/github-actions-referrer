ac09afda768a8fe9b55da4b068baa0850a858082
Jamaluddin Khan <jamaluddin.khan@metronlabs.com>
2023-01-09 13:08:12 +0530

    Flake8 Issue solved 1

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,6 @@
 def print_hello_name(name: str):
     print (f"Hello {name}")
 
-if __name__ == "__main__":
+if (__name__ == "__main__"):
     input_name = input("Enter the Name: ")
     print_hello_name(input_name)
